a8340f95244e6249d6e5b9debdf31fe498ae164b
cassioglay <cassio.glay#locktec.com.br>
2024-05-21 13:07:47 -0300

    chore: WatermelonDB -  add and config. watermelonDB

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: ['react-native-reanimated/plugin'],
+    "plugins": [["@babel/plugin-proposal-decorators", { "legacy": true }]]
+
   };
 };
